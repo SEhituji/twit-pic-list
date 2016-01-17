3e8504852a5baa46fbcc303e14bea0dd814e3410
hituji <hituji.engineer@gmail.com>
2016-01-18 05:34:23 +0900

    oauth処理を変更

--- callback.php
+++ callback.php
@@ -1,14 +1,31 @@
 <?php
 session_start();
-require_once ('config.php');
+require_once ('../config/config.php');
+require_once ('../vendor/autoload.php');
 
-// パラメータからoauth_verifierを取得
-$verifier = $_GET['oauth_verifier'];
+use Abraham\TwitterOAuth\TwitterOAuth;
 
-// Consumer key,Consumer secret, Request token, Request secretを使ってOAuthオブジェクト生成
-$to = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['request_token'], $_SESSION['request_token_secret']);
-// oauth_verifierを使ってAccess tokenを取得
-$access_token = $to->getAccessToken($verifier);
+//login.phpでセットしたセッション
+$request_token = array();  // [] は array() の短縮記法。詳しくは以下の「追々記」参照
+$request_token['oauth_token'] = $_SESSION['oauth_token'];
+$request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret'];
+
+//Twitterから返されたOAuthトークンと、あらかじめlogin.phpで入れておいたセッション上のものと一致するかをチェック
+if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
+    die( 'Error!' );
+}
+
+//OAuth トークンも用いて TwitterOAuth をインスタンス化
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+
+//アプリでは、access_token(配列になっています)をうまく使って、Twitter上のアカウントを操作していきます
+$_SESSION['access_token'] = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
+/*
+ちなみに、この変数の中に、OAuthトークンとトークンシークレットが配列となって入っています。
+*/
+
+//セッションIDをリジェネレート
+session_regenerate_id();
 
 // token keyとtoken secret, user_id, screen_nameをセッションに保存
 $_SESSION['oauth_token']        = $access_token['oauth_token'];
--- login.php
+++ login.php
@@ -1,14 +1,22 @@
 <?php
 session_start();
-require_once ('config.php');
+require_once ('../config/config.php');
+require_once ('../vendor/autoload.php');
 
-// OAuthオブジェクト生成
-$to = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-// callbackURLを指定してRequest tokenを取得
-$tok = $to->getRequestToken(" http://". GIP ."/twit/callback.php");
-// セッションに保存
-$_SESSION['request_token']=$token=$tok['oauth_token'];
-$_SESSION['request_token_secret'] = $tok['oauth_token_secret'];
-// AuthorizeURLを取得
-$url = $to->getAuthorizeURL($token);
-echo "<html><head><title>login</title></head><body><a href=\"". $url ."\">ログイン</a></body></html>";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//TwitterOAuth をインスタンス化
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+
+//コールバックURLをここでセット
+$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => GIP));
+
+//callback.phpで使うのでセッションに入れる
+$_SESSION['oauth_token'] = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+
+//Twitter.com 上の認証画面のURLを取得( この行についてはコメント欄も参照 )
+$url = $connection->url('oauth/authenticate', array('oauth_token' => $request_token['oauth_token']));
+
+//Twitter.com の認証画面へリダイレクト
+header( 'location: '. $url );
